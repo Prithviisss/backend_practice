15bf14f3b1500ce8a7cfd501fb80d417c70827ce
Prithvi <prithvis6002@gmail.com>
2026-04-19 18:17:49 +0530

    initial commit

---
+++ chapter3/src/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import path,{dirname} from 'path'
+import {fileURLToPath} from 'url'
+const app=express()
+const PORT=process.env.PORT||5005
+const __filename=fileURLToPath(import.meta.url)
+const __dirname=dirname(__filename)
+
+app.use(express.static(path.join(__dirname,'../public')))
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'public','index.html'))
+})
+console.log('hello world')
+
+app.listen(PORT,()=>{
+    console.log(`SERVER HAS STARTED ON:${PORT}`)
+})
