b39006afda75a94e1982b4de6c741480a8351dd3
Prithvi <prithvis6002@gmail.com>
2026-04-30 18:32:11 +0530

    middleware

--- chapter3/src/routes/authRoutes.js
+++ chapter3/src/routes/authRoutes.js
@@ -34,6 +34,21 @@ router.post('/register',(req,res)=>{
 
 } )
 router.post('/login',(req,res)=>{
+    const{username,password}=req.body
+    try{
+        const getUser=db.prepare('SELECT * FROM users WHERE username=?')
+        const user=getUser.get(username)
+        if(!user){
+            return res.status(404).send({message:"user not found"})
+        }
+        const passwordIsValid=bcrypt.compareSync(password,user.password)
+        if(!passwordIsValid){ return res.status(401).send({message:"invalid password"})}
+    }
+    catch(err){
+        console.log(err.message)
+        res.sendStatus(503)
+    }
+
     
 })
 
--- chapter3/src/routes/todoRoutes.js
+++ chapter3/src/routes/todoRoutes.js
@@ -5,6 +5,8 @@ const router =express.Router()
 
 router.get('/' ,(req,res)=>{ })
 
+router.post('/' ,(req,res)=>{ })
+
 router.put('/:id',(req,res)=>{})
 
 router.delete('/:id',(req,res)=>{ })
